ddbec69330f11da73541b53b5cc38e2d493bdaaa
Stella Karolina Nunes <stellakarolina.peixoto@gmail.com>
2025-04-03 11:46:31 -0300

    2️⃣ Criação das classes Cafe, CafeSimples e AdicionalDecorator

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
-// 1️⃣ Implementação do padrão Decorator em Dart
+/* 1️⃣ Implementação do padrão Decorator em Dart */
+
+// Interface base para o café
 abstract class Cafe {
   String descricao;
 
@@ -6,4 +8,27 @@ abstract class Cafe {
 
   String getDescricao() => descricao;
   double custo();
+}
+
+/*  2️⃣ Criação das classes Cafe, CafeSimples e AdicionalDecorator */
+
+// Implementação concreta do café simples
+class CafeSimples extends Cafe {
+  CafeSimples() : super("Café Simples");
+
+  @override
+  double custo() => 5.0;
+}
+
+// Decorator abstrato
+abstract class AdicionalDecorator extends Cafe {
+  final Cafe cafe;
+
+  AdicionalDecorator(this.cafe) : super(cafe.descricao);
+
+  @override
+  String getDescricao() => cafe.getDescricao();
+
+  @override
+  double custo() => cafe.custo();
 }
